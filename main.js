67400cb14e5e6de55761fd84ff4a97ebc1a387b3
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-20 15:55:28 +0100

    prova del get con ciclo for che restituisce 10 email

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ createApp({
     data() {
 
         return {
-           
+           emailbox :[""]
         
     }
     
@@ -12,5 +12,13 @@ createApp({
 
 methods :{
    
+},
+mounted (){
+
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta) {
+       for (let i = 0; i < 10; i++) {
+        console.log(risposta.data);
+       }
+    })
 }
 }).mount(`#app`);
