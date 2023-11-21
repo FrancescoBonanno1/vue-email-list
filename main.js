613df6a97445bb60b9da31a4040d39b350f23f8d
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-21 11:10:12 +0100

    aggiunta bottone con funzione genera email

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ createApp({
     data() {
         
         return {
-            arrayemail : []
+            arrayemail : [],
+            
     };
     
 },
 
 methods :{
+ generaemail(){
+     for (let i = 0; i < 10; i++) {
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risposta) =>{
+         this.arrayemail.push(risposta.data.response);
+         console.log(this.arrayemail);
+        })}
+    }
+
 },
 mounted (){
-  
-    for (let i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risposta) =>{
-        this.arrayemail.push(risposta.data.response);
-        console.log(this.arrayemail);
-            })}
+  generaemail()
    
 }
 }).mount(`#app`);
